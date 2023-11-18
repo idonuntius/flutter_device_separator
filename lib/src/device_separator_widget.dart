fc24ec8d4e90156b73f71337017653bedbe235b9
idonun <mc.natsu831@gmail.com>
2023-11-19 02:34:11 +0900

    add comment

--- lib/src/device_separator_widget.dart
+++ lib/src/device_separator_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_device_separator/src/device_type.dart';
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html show window;
 
+/// Widget that provides the ability to change widgets for each device.
 class DeviceSeparatorWidget extends StatelessWidget {
   const DeviceSeparatorWidget({
     super.key,
@@ -12,9 +13,17 @@ class DeviceSeparatorWidget extends StatelessWidget {
     required this.orElse,
   });
 
+  /// Specify widget for PC.
   final Widget? pc;
+
+  /// Specify widget for Tablet.
   final Widget? tablet;
+
+  /// Specify widget for Mobile.
   final Widget? mobile;
+
+  /// Specify widget for orElse.
+  /// If mobile is null and the user is using mobile, display this widget.
   final Widget orElse;
 
   @override
